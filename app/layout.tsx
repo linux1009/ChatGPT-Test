12d0a5020f7f3f7380486d77033cf075565bfdb0
linux1009 <367656@qq.com>
2023-04-28 22:54:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "隔壁老王的测试项目",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "隔壁老王的测试项目",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
